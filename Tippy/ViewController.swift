afd89ef2694f671c793d45d94bd6ddf51dea11d4
Dhriti <dhritichawla@dhritis-macbook-air.local>
2017-12-16 01:13:13 -0500

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Dhriti Chawla on 12/16/17.
+//  Copyright © 2017 Dhriti Chawla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
